2338ae72a83cbb0e914b2081d299de977e94e90e
Ethan Cornwill <ethancornwill@outlook.com>
2024-12-05 20:57:25 +1100

    add Watchlist component to display and manage user's movie watchlist

---
+++ src/pages/Watchlist.jsx
@@ -0,0 +1,45 @@
+import { useWatchlist } from '../context/WatchlistContext'
+import { Container, ListGroup, Button, Alert, Image } from 'react-bootstrap'
+
+export default function Watchlist () {
+  const { watchlist, removeFromWatchlist } = useWatchlist()
+
+  return (
+    <Container className='mt-5'>
+      <h1>Watchlist</h1>
+      {watchlist.length === 0
+        ? (
+          <Alert variant='info'>No movies in your watchlist.</Alert>
+          )
+        : (
+          <ListGroup>
+            {watchlist.map((movie) => (
+              <ListGroup.Item
+                key={movie.imdbID}
+                className='d-flex justify-content-between align-items-center'
+              >
+                <div className='d-flex align-items-center'>
+                  <Image
+                    src={movie.Poster !== 'N/A' ? movie.Poster : 'https://via.placeholder.com/50'}
+                    alt={movie.Title}
+                    rounded
+                    style={{ width: '50px', height: '75px', objectFit: 'cover', marginRight: '15px' }}
+                  />
+                  <div>
+                    <strong>{movie.Title}</strong> ({movie.Year})
+                  </div>
+                </div>
+                <Button
+                  variant='danger'
+                  size='sm'
+                  onClick={() => removeFromWatchlist(movie)}
+                >
+                  Remove
+                </Button>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+          )}
+    </Container>
+  )
+}
