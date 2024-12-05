052c58a03dbc0700edffadb390027fb564701011
Ethan Cornwill <ethancornwill@outlook.com>
2024-12-05 20:57:47 +1100

    add navigation and watchlist context to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,40 @@
-import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import Home from './pages/Home'
-
+import Watchlist from './pages/Watchlist'
+import WatchlistProvider from './context/WatchlistContext'
+import { Navbar, Nav, Container } from 'react-bootstrap'
 import './App.css'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
-function App () {
-  return (
-    <BrowserRouter>
+const App = () => (
+  <WatchlistProvider>
+    <Router>
+      <Navbar bg='dark' variant='dark' expand='lg'>
+        <Container>
+          <Navbar.Brand as={Link} to='/'>
+            MovieApp
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='me-auto'>
+              <Nav.Link as={Link} to='/'>
+                Home
+              </Nav.Link>
+              <Nav.Link as={Link} to='/watchlist'>
+                Watchlist
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/watchlist' element={<Watchlist />} />
       </Routes>
-    </BrowserRouter>
-  )
-}
+    </Router>
+    <ToastContainer position='top-right' autoClose={3000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover />
+  </WatchlistProvider>
+)
 
 export default App
